64852aec23effc102723b617b467cbcb36de7875
hexhamder <hexhamder@gmail.com>
2025-07-23 02:13:43 +0500

    Рашырение для Termux

---
+++ LICENSE.md
@@ -0,0 +1,2 @@
+ welcom.sh © 2025 by hexhamder распространяется по лицензии CC BY-ND 4.0.
+   Чтобы ознакомиться с условиями этой лицензии, посетите сайт https://creativecommons.org/licenses/by-nd/4.0/
---
+++ welcome.sh
@@ -0,0 +1,32 @@
+#!/data/com.termux/files/bin/bash
+
+#Version 1.0v beta
+
+clear
+echo -e '
+       .88888888:.   
+      88888888.88888. 
+    .8888888888888888. 
+   88._       _8888888 
+  88 88 88 88 88888888 
+  88_88_88 88888888 
+   .:88888:88888888:. 
+  .:88888:88888888:. 
+ .88888:88888888:888. 
+ 88888:88888888:88888 
+ 88888:88888888:88888 
+ 88888:88888888:88888 
+ `8888:88888888:8888´ 
+  `888:88888888:888´
+   `88iiiiiiiiii:88` 
+     `88:88888888:88´ 
+       `888888888´ 
+
+\e[1;34m   R E L U N X - A N D R O I D\e[Om
+\e[1;37mBy: @Hexhamder\e[Om
+\e[1;31mTelegram: https://t.me/hexhamder\e[Om
+
+\e[1;37mGmail: hexhamder@gmail.cim\e[Om
+'
+#welcom.sh © 2025 by hexhamder распространяется по лицензии CC BY-ND 4.0.
+#Чтобы ознакомиться с условиями этой лицензии, посетите сайт https://creativecommons.org/licenses/by-nd/4.0/
